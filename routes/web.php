d1aac1c7d95b89c8b0924433adfc26e0d92c6fe3
ast202Ast <axelhouwt@gmail.com>
2025-01-05 23:39:45 +0100

    [Notes] Correction de bugs

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NoteController;
+use App\Http\Controllers\UnitesEnseignementController;
+use App\Http\Controllers\ElementsConstitutifsController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +19,22 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('notes/create', [NoteController::class, 'create'])->name('notes.create');
+
+Route::post('notes', [NoteController::class, 'store'])->name('notes.store');
+
+Route::resource('unites-enseignement', UnitesEnseignementController::class)->names([
+    'index' => 'unites-enseignement.index',
+    'create' => 'unites-enseignement.create',
+    'store' => 'unites-enseignement.store',
+    'show' => 'unites-enseignement.show',
+    'edit' => 'unites-enseignement.edit',
+    'update' => 'unites-enseignement.update',
+    'destroy' => 'unites-enseignement.destroy',
+]);
+// Route::resource('/unites-enseignement',UnitesEnseignementController::class)->names('unites-enseignement');
+Route::resource('elements-constitutifs', ElementsConstitutifsController::class)->names('elements-constitutifs');
+// Route::delete('/unites-enseignement/{id}', [UnitesEnseignementController::class, 'destroy'])->name('unites-enseignement.delete');
 
 require __DIR__.'/auth.php';
